57b59278ce9aeab0e4a796cb5cab43f7cb50877f
김지민 <samjin123kr@gmail.com>
2022-02-07 22:30:53 +0900

    [Study] Multi Project 공부중
    Study Multi Project

--- sp/files/src/main/java/com/sp/unitt/files/domain/Person.java
+++ sp/files/src/main/java/com/sp/unitt/files/domain/Person.java
@@ -1,11 +1,16 @@
 package com.sp.unitt.files.domain;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Person {
     private String name;
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Person.class, args);
         System.out.println("234234");
     }
 
